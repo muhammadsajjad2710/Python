c33985b6c11fb682c61caaeb0246f06e428f1303
Muhammad Sajjad <86406956+muhammadsajjad2710@users.noreply.github.com>
2024-11-25 00:39:53 +0500

    Add files via upload

---
+++ Python Strings/strings.py
@@ -0,0 +1,33 @@
+# # # # # # var1  = input("Enter the string : " )
+# # # # # # var2  = input("Enter the string : " )
+
+# # # # # # print ("var1[0]: ",var1[0])
+# # # # # # print ("var2[1:4]: ",var2[1:4])
+
+# # # # # var1 = "Hello World"
+# # # # # print("Updated Sting :- ", var1[:6] + 'Python')
+# # # # print ( " my name is %s and i my age is %d "% ('ali', 23))
+
+# # # # Double Quotes in Python Strings
+
+# # # var = 'I am very good player but "Ali" is excellent player'
+# # # print("var :",var)
+
+# # # Triple Quotes
+# # var = '''alasioshuouw'''
+# # print(var)
+# # var2 = """aoduhohsdfis"""
+# # print(var2)
+
+# # Python Multiline Strings
+# var2= '''klsklks
+# sosoos
+# pjiososj
+# asso'''
+# print(var2)
+# # Arithmetic Operators with Strings
+# print("hello"*"word")
+# Getting Type of Python Strings
+
+string = "osossss"
+print(type(string))
