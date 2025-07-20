c928189730a57eba0b90ca8b1dcee857cd0a5add
Muhammad Sajjad <86406956+muhammadsajjad2710@users.noreply.github.com>
2025-07-21 02:36:05 +0500

    Add files via upload

---
+++ Django/login_authentication/login/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import redirect
+
+def handleSignUp(request):
+    if request.method=="POST":
+        # Get the post parameters
+        username=request.POST['username']
+        email=request.POST['email']
+        fname=request.POST['fname']
+        lname=request.POST['lname']
+        pass1=request.POST['pass1']
+        pass2=request.POST['pass2']
+
+        # check for errorneous input
+        if len(username)<10:
+            messages.error(request, " Your user name must be under 10 characters")
+            return redirect('home')
+
+        if not username.isalnum():
+            messages.error(request, " User name should only contain letters and numbers")
+            return redirect('home')
+        if (pass1!= pass2):
+             messages.error(request, " Passwords do not match")
+             return redirect('home')
+        
+        # Create the user
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name= fname
+        myuser.last_name= lname
+        myuser.save()
+        messages.success(request, " Your iCoder has been successfully created")
+        return redirect('home')
+
+    else:
+        return HttpResponse("404 - Not found")
